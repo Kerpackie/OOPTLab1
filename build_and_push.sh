1b36f71aee38a25645d8b85a5a2c8cb7458f2414
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2023-09-30 23:05:52 +0100

    Update build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,12 +3,19 @@
 set -e
 
 cd $CONTEXT_DIR
-rm /tmp/build_args || echo OK
-env >/tmp/build_args
-echo "--build-arg \""$(cat /tmp/build_args | sed -z 's/\n/" --build-arg "/g')"IGNORE_VAR=IGNORE_VAR\"" >/tmp/build_args
-BUILD_ARGS=$(cat /tmp/build_args)
-COMMAND="docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest -f $DOCKERFILE $BUILD_ARGS --no-cache ."
-/bin/bash -c "$COMMAND"
+
+# Create the build_args file
+env > /tmp/build_args
+
+# Format the build arguments
+BUILD_ARGS=$(cat /tmp/build_args | sed -z 's/\n/ --build-arg /g')
+
+# Build and tag the Docker image
+docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest -f $DOCKERFILE --build-arg $BUILD_ARGS --no-cache .
+
+# Push the Docker images
 docker push $IMAGE_NAME_WITH_REGISTRY:latest
 docker push $FULL_IMAGE_NAME
+
+# Remove the temporary build_args file
 rm /tmp/build_args
